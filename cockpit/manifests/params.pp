acb19b480ee2b0fa1174fb0d8157b4782ed4a071
Afeef Ghannam <39904920+afeefghannam89@users.noreply.github.com>
2018-10-15 13:01:33 +0200

    Update params.pp

--- cockpit/manifests/params.pp
+++ cockpit/manifests/params.pp
@@ -1,6 +1,6 @@
 # cockpit::params - Default parameters
 class cockpit::params {
-
+ 
  # OS Specific Defaults
   case $::osfamily {
     'RedHat': {
@@ -15,23 +15,23 @@ class cockpit::params {
       fail("${::operatingsystem} not supported")
     }
   }
-
+ 
   # Defaults for all Operating Systems
   # (Cockpit has consistant naming accross OS's, hooray! :D)
-  $allowunencrypted = false
-  $logintitle       = $::fqdn
-  $manage_package   = true
-  $manage_repo      = true
-  $manage_service   = true
-  $maxstartups      = '10'
-  $package_name     = ['cockpit', 'cockpit-bridge', 'cockpit-dashboard', 'cockpit-doc', 'cockpit-ws', 'cockpit-docker', 'cockpit-kubernetes', 'cockpit-machines', 'cockpit-machines-ovirt.noarch', 'cockpit-packagekit', 'cockpit-pcp', 'cockpit-selinux.noarch', 'setroubleshoot', 'setools', 'cockpit-storaged.noarch', 'cockpit-tests','sos', 'subscription-manager-cockpit.noarch']
-  $package_version  = 'installed'
-  $port             = undef 
-  $service_ensure   = 'running'
-  $service_enable   = true
-  $service_name     =  ['cockpit.socket', 'cockpit']
+  $allowunencrypted     = false
+  $logintitle           = $::fqdn
+  $manage_package       = true
+  $manage_repo          = true
+  $manage_service       = true
+  $maxstartups          = '10'
+  $package_name         = ['cockpit', 'cockpit-bridge', 'cockpit-dashboard', 'cockpit-doc', 'cockpit-ws', 'cockpit-docker', 'cockpit-kubernetes', 'cockpit-machines', 'cockpit-machines-ovirt.noarch', 'cockpit-packagekit', 'cockpit-pcp', 'cockpit-selinux.noarch', 'setroubleshoot', 'setools', 'cockpit-storaged.noarch', 'cockpit-tests','sos', 'subscription-manager-cockpit.noarch']
+  $package_version      = 'installed'
+  $port                 = undef
+  $service_ensure       = 'running'
+  $service_enable       = true
+  $service_name         =  ['cockpit.socket']
   $manage_kdump_service = true
-  $kdump_service_name = 'kdump'
+  $kdump_service_name   = 'kdump'
   $kdump_service_ensure = 'running'
   $kdump_service_enable = true
 }
